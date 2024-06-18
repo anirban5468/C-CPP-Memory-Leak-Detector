3e406fac8e078050c1077cb2377d7113643ef8d0
Anirban De <anirban5468@gmail.com>
2024-06-18 13:22:23 +0530

    mld.c

--- mld.c
+++ mld.c
@@ -1,5 +1,7 @@
+/* mld.c */
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "mld.h"
 #include <assert.h>
 #include <memory.h>
@@ -11,47 +13,49 @@ char *DATA_TYPE[] = {"UINT8", "UINT32", "INT32",
 /* Dumping Function */
 void print_structure_rec(struct_db_rec_t *struct_rec)
 {
-    if(!struct_rec) return;
+    if (!struct_rec)
+        return;
 
     int j = 0;
-    
+
     field_info_t *field = NULL;
-    
+
     printf("|------------------------------------------------------|\n");
     printf("| %-20s | size = %-8d | #flds = %-3d |\n", struct_rec->struct_name, struct_rec->ds_size, struct_rec->n_fields);
     printf("|------------------------------------------------------|------------------------------------------------------------------------------------------|\n");
-    
-    for(j = 0 ; j < struct_rec->n_fields ; j++)
+
+    for (j = 0; j < struct_rec->n_fields; j++)
     {
         field = &struct_rec->fields[j];
 
         printf("  %-20s |", "");
         printf("%-3d %-20s | dtype = %-15s | size = %-5d | offset = %-6d|  nstructname = %-20s  |\n",
-                j, field->fname, DATA_TYPE[field->dtype], field->size, field->offset, field->nested_str_name);
+               j, field->fname, DATA_TYPE[field->dtype], field->size, field->offset, field->nested_str_name);
         printf("  %-20s |", "");
         printf("--------------------------------------------------------------------------------------------------------------------------|\n");
     }
 }
 
 void print_structure_db(struct_db_t *struct_db)
-{    
-    if(!struct_db) return;
- 
-    printf("Printing STRUCURE DATABASE : \n\n");
- 
+{
+    if (!struct_db)
+        return;
+
+    printf("Printing STRUCTURE DATABASE : \n\n");
+
     int i = 0;
- 
+
     struct_db_rec_t *struct_rec = NULL;
     struct_rec = struct_db->head;
- 
+
     printf("No of Structures Registered = %d\n\n", struct_db->count);
- 
-    while(struct_rec)
+
+    while (struct_rec)
     {
-        printf("structure No : %d (%p)\n", i++, struct_rec);    
+        printf("structure No : %d (%p)\n", i++, struct_rec);
         print_structure_rec(struct_rec);
         printf("\n");
-    
+
         struct_rec = struct_rec->next;
     }
 }
@@ -60,49 +64,49 @@ int add_structure_to_struct_db(struct_db_t *struct_db, struct_db_rec_t *struct_r
 {
     struct_db_rec_t *head = struct_db->head;
 
-    if(!head)
+    if (!head)
     {
         struct_db->head = struct_rec;
         struct_rec->next = NULL;
         struct_db->count++;
- 
+
         return 0;
     }
 
     struct_rec->next = head;
     struct_db->head = struct_rec;
     struct_db->count++;
- 
+
     return 0;
 }
 
-
 static struct_db_rec_t *struct_db_look_up(struct_db_t *struct_db, char *struct_name)
-{    
+{
     struct_db_rec_t *head = struct_db->head;
- 
-    if(!head) return NULL;
-    
-    for(; head; head = head->next)
+
+    if (!head)
+        return NULL;
+
+    for (; head; head = head->next)
     {
-        if(strncmp(head->struct_name, struct_name, MAX_STRUCTURE_NAME_SIZE) == 0)
-        return head;
+        if (strncmp(head->struct_name, struct_name, MAX_STRUCTURE_NAME_SIZE) == 0)
+            return head;
     }
 
     return NULL;
 }
 
-
 static object_db_rec_t *object_db_look_up(object_db_t *object_db, void *ptr)
 {
     object_db_rec_t *head = object_db->head;
- 
-    if(!head) return NULL;
-    
-    for(; head; head = head->next)
+
+    if (!head)
+        return NULL;
+
+    for (; head; head = head->next)
     {
-        if(head->ptr == ptr)
-        return head;
+        if (head->ptr == ptr)
+            return head;
     }
 
     return NULL;
@@ -110,10 +114,10 @@ static object_db_rec_t *object_db_look_up(object_db_t *object_db, void *ptr)
 
 /* Working with objects */
 static void add_object_to_object_db(object_db_t *object_db, void *ptr, int units, struct_db_rec_t *struct_rec, mld_boolean_t is_root)
-{     
+{
     object_db_rec_t *obj_rec = object_db_look_up(object_db, ptr);
- 
-    /* Dont add same object twice */
+
+    /* Don't add the same object twice */
     assert(!obj_rec);
 
     obj_rec = calloc(1, sizeof(object_db_rec_t));
@@ -126,13 +130,13 @@ static void add_object_to_object_db(object_db_t *object_db, void *ptr, int units
     obj_rec->is_root = is_root;
 
     object_db_rec_t *head = object_db->head;
-        
-    if(!head)
+
+    if (!head)
     {
         object_db->head = obj_rec;
         obj_rec->next = NULL;
         object_db->count++;
-    
+
         return;
     }
 
@@ -141,81 +145,17 @@ static void add_object_to_object_db(object_db_t *object_db, void *ptr, int units
     object_db->count++;
 }
 
-
 void *xcalloc(object_db_t *object_db, char *struct_name, int units)
 {
     struct_db_rec_t *struct_rec = struct_db_look_up(object_db->struct_db, struct_name);
- 
+
     assert(struct_rec);
- 
+
     void *ptr = calloc(units, struct_rec->ds_size);
- 
-    add_object_to_object_db(object_db, ptr, units, struct_rec, MLD_FALSE);  /* xmalloc by default set the object as non-root */
- 
+
+    add_object_to_object_db(object_db, ptr, units, struct_rec, MLD_FALSE); /* xcalloc by default sets the object as non-root */
+
     return ptr;
 }
 
-
-static void delete_object_record_from_object_db(object_db_t *object_db, object_db_rec_t *object_rec)
-{
-    assert(object_rec);
-
-    object_db_rec_t *head = object_db->head;
- 
-    if(head == object_rec)
-    {
-        object_db->head = object_rec->next;
-    
-        free(object_rec);
-    
-        return;
-    }
-    
-    object_db_rec_t *prev = head;
-    head = head->next;
-
-    while(head)
-    {
-        if(head != object_rec)
-        {
-            prev = head;
-            head = head->next;
-        
-            continue;
-        }
-
-        prev->next = head->next;
-        head->next = NULL;
-        
-        free(head);
-        
-        return;
-    }
-}
-
-
-void xfree(object_db_t *object_db, void *ptr)
-{
-    if(!ptr) return;
- 
-    object_db_rec_t *object_rec = object_db_look_up(object_db, ptr);
-        
-    assert(object_rec);
-    assert(object_rec->ptr);
-
-    free(object_rec->ptr);
-    
-    object_rec->ptr = NULL;
-    
-    /* Delete object record from object db */
-    
-    delete_object_record_from_object_db(object_db, object_rec);
-}
-
-
-/* Dumping Functions for Object database */
-void print_object_rec(object_db_rec_t *obj_rec, int i)
-{    
-    if(!obj_rec) return;
- 
-    printf
+static void delete_object_record_from_object_db(object_db_t *object
